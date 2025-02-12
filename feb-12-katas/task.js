99395979eea84f333502c34b666c1523b1152a5c
Moses Oghene <mosesogheneo@gmail.com>
2025-02-12 16:43:36 +0100

    tasks kata

---
+++ feb-12-katas/task.js
@@ -0,0 +1,23 @@
+let filterScore = (studentScores, threshold) => studentScores.filter((studentScore) => studentScore >= threshold);
+let increaseScoreBy = (studentScores, number) => studentScores.map((studentScore) => studentScore + number);
+let getSquares = (studentScores) => studentScores.map((studentScore) => studentScore * studentScore);
+
+let sendBookToAll = (members, book) => {
+    let sentMembers = {};
+    for (const member of members) {
+        sentMembers[member] = book;
+    }
+    return sentMembers;
+}
+
+let findNoonClasses = (classesSchedule) => classesSchedule.filter((schedule) => schedule.includes("PM") || schedule.includes("pm"));
+let totalOf = (expenses) => {
+    let total = 0;
+    for (const category in expenses) {
+        total += expenses[category];
+    }
+    return total;
+}
+
+
+module.exports = {filterScore, increaseScoreBy, getSquares, sendBookToAll, findNoonClasses, totalOf};
---
+++ feb-12-katas/task.test.js
@@ -0,0 +1,48 @@
+const {filterScore, increaseScoreBy, getSquares, sendBookToAll, findNoonClasses, totalOf} = require("./task");
+
+beforeEach(() => studentScores = [10, 30, 50, 70, 90, 80, 100, 11]);
+
+afterEach(() => studentScores = [10, 30, 50, 70, 90, 80, 100, 11]);
+
+test("filter scores greater than 70",() => {
+    let result = filterScore(studentScores, 70);
+    let answer = [70, 90, 80, 100];
+    expect(result).toEqual(answer);
+});
+
+test("increase scores by 5",() => {
+    let result = increaseScoreBy(studentScores, 5);
+    let answer = [15, 35, 55, 75, 95, 85, 105, 16];
+    expect(result).toEqual(answer);
+});
+
+test("get squares of number",() => {
+    let result = getSquares(studentScores);
+    let answer = [100, 900, 2500, 4900, 8100, 6400, 10000, 121];
+    expect(result).toEqual(answer);
+});
+
+test("Send book to all members",() => {
+    let result = sendBookToAll(["Leke", "Divi", "Esth", "Uzor"], "How to kill a Mockingbird");
+    let answer = {"Divi": "How to kill a Mockingbird", "Esth": "How to kill a Mockingbird", "Leke": "How to kill a Mockingbird", "Uzor": "How to kill a Mockingbird"};
+    expect(result).toEqual(answer);
+});
+
+test("test fetch all noon classes", () => {
+    let result = findNoonClasses(["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM"]);
+    let answer = ["1:00 PM", "3:00 PM", "5:00 PM"];
+    expect(result).toEqual(answer);
+});
+
+test("test fetch total of groceries", () => {
+    let data = {
+        'groceires': 150,
+        'dining out': 100,
+        'transportation': 50,
+        'entertainment': 80
+    };
+    let result = totalOf(data);
+    let answer = 380;
+    expect(result).toBe(answer);
+});
+
